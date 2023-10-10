b24a593b652983f1b5224cee54e4bdfbce788735
rashikapr <74289013+rashikapr@users.noreply.github.com>
2023-10-10 17:45:20 +0530

    Update markerHandler.js

--- js/markerHandler.js
+++ js/markerHandler.js
@@ -121,31 +121,7 @@ AFRAME.registerComponent("markerhandler", {
   },
   handleOrder: function (tNumber, dish) {
     // Reading current table order details
-    firebase
-      .firestore()
-      .collection("tables")
-      .doc(tNumber)
-      .get()
-      .then(doc => {
-        var details = doc.data();
-
-        if (details["current_orders"][dish.id]) {
-          // Increasing Current Quantity
-          details["current_orders"][dish.id]["quantity"] += 1;
-
-          //Calculating Subtotal of item
-          var currentQuantity = details["current_orders"][dish.id]["quantity"];
-
-          details["current_orders"][dish.id]["subtotal"] =
-            currentQuantity * dish.price;
-        } else {
-          details["current_orders"][dish.id] = {
-            item: dish.dish_name,
-            price: dish.price,
-            quantity: 1,
-            subtotal: dish.price * 1
-          };
-        }
+    
 
         details.total_bill += dish.price;
 
